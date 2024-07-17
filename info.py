5ba6d54991f13ecb356b21b9a85e301b1fc5f361
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2024-07-17 10:26:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/How_to_Open0")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-100215271207'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002152712072'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6739173851').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-100223041406').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
